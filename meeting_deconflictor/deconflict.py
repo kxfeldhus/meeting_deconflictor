13957599de65ba7645d08afe17323c7134fbbb8d
kfeldhus <kfeldhus@tendrilinc.com>
2018-10-08 17:48:21 -0600

    Add some more comments and todos. Make the 'report' a bit prettier.  But not by much ;)

--- meeting_deconflictor/deconflict.py
+++ meeting_deconflictor/deconflict.py
@@ -7,8 +7,13 @@ class Deconflict:
     """ This class holds the core conflict detection logic. """
 
     def __init__(self, reader):
-        # Accept a reader so we can easily switch between different reader formats, like xml or json.
+        """ Accept a reader so we can easily switch between different reader formats, like xml or json.
+            reader must implement a #read() method that returns a List of Meetings.
+        """
+        # TODO: Make sure the reader has a #read() method.
+
         self.reader = reader
+
 
     def find_conflicts(self):
         """ Read the file and return a List of meetings with conflicts. """
@@ -28,16 +33,29 @@ class Deconflict:
             # Core overlap detection.  If the end time of the current meeting is after the start time of the next
             #  meeting then we have a conflict.
             if current_meeting.end_time > next_meeting.start_time:
-                conflicted_meetings.extend([current_meeting, next_meeting])
+                conflicted_meetings.append((current_meeting, next_meeting))
 
         return conflicted_meetings
 
     @classmethod
     def generate_report_for_file(cls, reader):
+        """ A utility method to be used as the primary entry point to generate a report from an input reader. """
         deconflictor = Deconflict(reader)
         conflicts = deconflictor.find_conflicts()
 
-        print(conflicts)
+        Deconflict.report_conflicts(conflicts)
+
+    @classmethod
+    def report_conflicts(cls, conflicts):
+        """ Print out the conflicts. This is a very simple example to iterate on.
+            A future improvement would be to have different reporter classes that could
+            report out in various ways and formats.  Like to stdout here, or to a file, or send an email, or publish to
+            a web service.
+        """
+        print("Conflict Report:")
+        for conflict1, conflict2 in conflicts:
+            print(f'{conflict1} --- conflicts with --- {conflict2}')
+
 
 
 if __name__ == "__main__":
