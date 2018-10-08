4aecbcb8d63a35266c83f9130c7fa7a87fcf9048
kfeldhus <kfeldhus@tendrilinc.com>
2018-10-08 17:00:31 -0600

    Beef up the testing, add some comments to the meeting class.

--- meeting_deconflictor/meeting.py
+++ meeting_deconflictor/meeting.py
@@ -1,13 +1,27 @@
 from datetime import datetime
 
-TIME_FORMAT = "%I:%M%p"
+TIME_FORMAT = "%I:%M%p"  # Format for times that drops the date.
 
 
 class Meeting:
+    """ This class holds the data, validation, and the sorting logic for a Meeting. """
 
     def __init__(self, start_time, end_time):
-        self.start_time = datetime.strptime(start_time, TIME_FORMAT)
-        self.end_time = datetime.strptime(end_time, TIME_FORMAT)
+        """ A new Meeting object, will throw a ValueError on bad time values. """
+        start_time = datetime.strptime(start_time, TIME_FORMAT)
+        end_time = datetime.strptime(end_time, TIME_FORMAT)
+
+        if Meeting.validate_time_args(start_time, end_time):
+            self.start_time = start_time
+            self.end_time = end_time
+        else:
+            raise ValueError(f"start time of {start_time} must be before {end_time}")
+
+    @staticmethod
+    def validate_time_args(start_time, end_time):
+        """ Validate that the start time is before the end time of a meeting """
+
+        return start_time < end_time
 
     def __repr__(self):
         start_time = datetime.strftime(self.start_time, TIME_FORMAT)
@@ -18,7 +32,7 @@ class Meeting:
         return self.start_time == other.start_time and self.end_time == other.end_time
 
     def __lt__(self, other):
-        return self.end_time < other.end_time
+        return self.start_time < other.start_time
 
     def __gt__(self, other):
-        return self.end_time > other.end_time
+        return self.start_time > other.start_time
--- tests/test_meeting.py
+++ tests/test_meeting.py
@@ -1,11 +1,13 @@
 from datetime import datetime
 
+import pytest
+
 from meeting_deconflictor.meeting import Meeting, TIME_FORMAT
 
 
 class TestMeeting:
 
-    def test_creation(self):
+    def test_successful_creation(self):
         meeting = Meeting("6:00am", "7:00am")
 
         expected_start_time = datetime.strptime("6:00am", TIME_FORMAT)
@@ -13,4 +15,23 @@ class TestMeeting:
 
         assert meeting.start_time == expected_start_time
         assert meeting.end_time == expected_end_time
+
+    def test_throws_exception_on_start_time_after_end_time(self):
+        with pytest.raises(ValueError) as ex:
+            Meeting("7:00am", "6:00am")
+
+        assert 'must be before' in str(ex)
+
+    def test_throws_exception_on_start_time_equal_to_end_time(self):
+        with pytest.raises(ValueError) as ex:
+            Meeting("7:00am", "7:00am")
+
+        assert 'must be before' in str(ex)
+
+    def test_throws_exception_on_invalid_time(self):
+        with pytest.raises(ValueError):
+            Meeting("27:00am", "6:00am")
+
+        with pytest.raises(ValueError):
+            Meeting("7:00am", "x")
 
